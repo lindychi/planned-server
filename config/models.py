851aecbc83f8d2a191dc4d5f64e880b8ad17cb35
hanchi <igam0000@gmail.com>
2021-03-21 09:05:09 +0900

    color model update

--- config/models.py
+++ config/models.py
@@ -6,3 +6,9 @@ from django.conf import settings
 class Config(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     show_todo_complete = models.BooleanField(default=True)
+
+class Color(models.Model):
+    color = models.CharField(max_length=12)
+    bg_color = models.CharField(max_length=12)
+    drag_bg_color = models.CharField(max_length=12)
+    border_color = models.CharField(max_length=12)
