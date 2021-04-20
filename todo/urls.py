f3e8dac70baa266e805ea67ba93fd0fd6c3d9e86
lindychi <um0205@naver.com>
2021-04-21 00:36:23 +0900

    4 todo GitHub link (#5)
    * #4 todo github link
    
    * #4 migrations file
    
    * #4 underline fix
    
    Co-authored-by: hanchi <igam0000@gmail.com>

--- todo/urls.py
+++ todo/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('add_new_schedule/<int:tid>/', views.add_new_schedule, name='add_new_schedule'),
     path('end_last_schedule/<int:tid>/', views.end_last_schedule, name='end_last_schedule'),
     path('todo/detail/<int:tid>/', views.todo_detail, name="todo_detail"),
+    path('todo/set_github_repo/', views.set_github_repo, name="set_github_repo"),
 
     path('ajax/complete/', views.complete_todo, name='complete_todo'),
     path('ajax/delete_todo/', views.delete_todo, name='delete_todo'),
