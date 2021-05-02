33fab9eb94c5c08efef7fe0d1910b5d6d4bb83d7
lindychi <um0205@naver.com>
2021-05-02 14:36:56 +0900

    12 wiki (#14)
    * #8 fixed installment update
    
    * #12 add wiki app
    
    * #12 wiki default menu update
    
    Co-authored-by: hanchi <igam0000@gmail.com>

---
+++ wiki/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'wiki'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_wiki', views.add_wiki, name='add_wiki'),
+    path('detail_wiki/<int:wid>/', views.detail_wiki, name='detail_wiki'),
+]
---
+++ wiki/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Wiki
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+# Create your views here.
+@login_required
+def index(request):
+    wikis = Wiki.objects.filter(user=request.user)
+    return render(request, 'wiki/index.html', {'wikis':wikis})
+
+@login_required
+def add_wiki(request):
+    if request.method == "POST":
+        wiki = Wiki.objects.create(user=request.user, title=request.POST['title'], content=request.POST['content'])
+        return reverse('wiki:index')
+    return render(request, 'wiki/add_wiki.html', {})
+
+@login_required
+def detail_wiki(request, wid):
+    wiki = Wiki.objects.get(user=request.user, id=wid)
+    return render(request, 'wiki/detail_wiki.html', {'wiki':wiki})
