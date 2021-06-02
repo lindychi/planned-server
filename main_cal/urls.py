7868a5ff0d4ab3752b86539425ea896653642dfa
lindychi <um0205@naver.com>
2021-06-03 07:24:39 +0900

    #24 delete button work (#42)

--- main_cal/urls.py
+++ main_cal/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('calendar/<int:cid>/change_random_color/', views.change_random_color, name='change_random_color'),
 
     path('ajax_edit_schedule/', views.ajax_edit_schedule, name='ajax_edit_schedule'),
+    path('ajax_delete_schedule/', views.ajax_delete_schedule, name='ajax_delete_schedule'),
 ]
